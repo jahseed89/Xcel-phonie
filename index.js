947f5b2db43fcb0de0f181dc1968cb29d3d5f473
jahseed89 <samson.ocran@gmail.com>
2022-07-26 20:43:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const verifyBtn = document.getElementById('verify-btn');
+const phoneNumber = document.getElementById('phonenumber');
+// ******MTN Numbers************
+const mtnNumbers = ["0702", "0703","0704","0706","0803","0806","0810","0813","0814","0816","0903","0906","0913"]
+// ******Airtel Numbers*****
+const airtelNumbers = ["0802","0904", "0808","0701","0708","0901","0812"]
+// *****Glo Numbers**********
+const gloNumbers = ['0805','0807','0705','0815','0811','0905']
+// ******9mobile********
+const etisalatNumbers = ['0817','0818','0809']
+
+const mtnLogo = "https://www.logolynx.com/images/logolynx/58/5851155e37656ef98b095e8517ebb5fc.jpeg"
+const airtelLogo = "https://seeklogo.com/images/A/airtel-logo-439F62AEA0-seeklogo.com.png"
+const etisalatLogo = "https://www.seekpng.com/png/detail/344-3443327_9mobile-mtn-glo-airtel-and-9mobile.png"
+const gloLogo = "https://www.logolynx.com/images/logolynx/85/85c703ba97ca1bee01c977f59047f67b.jpeg"
+
+let numberType = null
+const result = document.getElementById("result")
+const updateResult =(value)=>{
+    result.innerHTML = `<img src="${value}" />`
+    result.style.visibility = "visible"
+}
+
+const checkNumber = ()=>{
+    let number = phoneNumber.value;
+    if(number.length <= 4)
+        return
+    const firstFour = number.slice(0,4)
+    if(mtnNumbers.includes(firstFour)){
+        numberType  = mtnLogo;
+        updateResult(numberType)
+    }
+    else if(airtelNumbers.includes(firstFour)){
+        numberType = airtelLogo;
+        updateResult(numberType)
+    }
+    else if(gloNumbers.includes(firstFour)){
+        numberType = gloLogo;
+        updateResult(numberType)
+    }
+    else if(etisalatNumbers.includes(firstFour)){
+        numberType = etisalatLogo;
+        updateResult(numberType)
+    }
+    else{
+        numberType = "INVALID"
+        result.innerHTML = numberType
+    }
+    
+}
+verifyBtn.addEventListener('click', checkNumber)
+// phoneNumber.addEventListener('keydown', checkNumber)
