17e86d987f3a9894a0cbbe2950ddf2531bcf913c
jahseed89 <samson.ocran@gmail.com>
2022-07-31 21:57:40 +0100

    select imput filed updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const verifyBtn = document.getElementById('verify-btn');
 const phoneNumber = document.getElementById('phonenumber');
+const mobileHolder = document.getElementById('mobileoperator');
 // ******MTN Numbers************
 const mtnNumbers = ["0702", "0703","0704","0706","0803","0806","0810","0813","0814","0816","0903","0906","0913"]
 // ******Airtel Numbers*****
@@ -9,6 +10,9 @@ const gloNumbers = ['0805','0807','0705','0815','0811','0905']
 // ******9mobile********
 const etisalatNumbers = ['0817','0818','0809']
 
+let validationList =null
+
+
 const mtnLogo = "https://www.logolynx.com/images/logolynx/58/5851155e37656ef98b095e8517ebb5fc.jpeg"
 const airtelLogo = "https://seeklogo.com/images/A/airtel-logo-439F62AEA0-seeklogo.com.png"
 const etisalatLogo = "https://www.seekpng.com/png/detail/344-3443327_9mobile-mtn-glo-airtel-and-9mobile.png"
@@ -16,16 +20,66 @@ const gloLogo = "https://www.logolynx.com/images/logolynx/85/85c703ba97ca1bee01c
 
 let numberType = null
 const result = document.getElementById("result")
+let logo = null
 const updateResult =(value)=>{
     result.innerHTML = `<img src="${value}" />`
     result.style.visibility = "visible"
 }
+// ******Checking the selected network**************
+const checkNetwork = (e)=>{
+   
+     let currentNetwork = e.target.value
+     if(currentNetwork === "MTN"){
+        validationList = mtnNumbers
+        logo = mtnLogo  
+     }else if(currentNetwork === "Airtel"){
+        validationList = airtelNumbers
+        logo = airtelLogo
+     }
+     else if(currentNetwork === "Glo"){
+        validationList = gloNumbers
+        logo = gloLogo
+     }
+     else if(currentNetwork === "9mobile"){
+        validationList = etisalatNumbers
+        logo = etisalatLogo
+     }else{
+        validationList = "all"
+     }
 
-const checkNumber = ()=>{
-    let number = phoneNumber.value;
-    if(number.length <= 4)
+}
+const handleClick = () =>{
+    checkNumber(phoneNumber.value)
+}
+const checkNumber = (value)=>{
+    let number = value;
+    if(!validationList){
         return
+    }
+    if(number.length <= 4) 
+        return
+    if(number.slice(0,4) == "+234"){
+        number ="0"+number.slice(4)
+    }
+
     const firstFour = number.slice(0,4)
+    if(validationList !== "all"){
+    if(validationList.includes(firstFour)){
+        numberType  = logo;
+        updateResult(numberType)
+        
+    }
+    else{
+        numberType = "INVALID NUMBER OR WRONG NETWORK"
+        result.innerHTML = numberType
+    }
+    }
+    else{
+        allValidate(numberType)
+    }
+    
+}
+const allValidate = (firstFour) =>{
     if(mtnNumbers.includes(firstFour)){
         numberType  = mtnLogo;
         updateResult(numberType)
@@ -46,7 +100,7 @@ const checkNumber = ()=>{
         numberType = "INVALID"
         result.innerHTML = numberType
     }
-    
 }
-verifyBtn.addEventListener('click', checkNumber)
-// phoneNumber.addEventListener('keydown', checkNumber)
+
+mobileHolder.addEventListener('change', checkNetwork);
+verifyBtn.addEventListener('click', handleClick);
